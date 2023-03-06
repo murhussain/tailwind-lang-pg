35833aecd0828e609cc4481ff111cac4f8c2ba7e
Murhussain <husseinmurashi@yahoo.com>
2023-03-06 18:31:30 +0200

    Working on hero section at 80%

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       colors: {
         'body': '#f6f6f6',
+        'black': '#0d0103',
+        'accent-black': '#3d3435',
+        'hero-body': '#f3e7e9',
+        'gray': '#c5c5c5',
         'gray-accent': '#989898',
         'primary': '#870D22',
       }
