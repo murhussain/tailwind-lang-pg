d9b16adc9f569779be56fa224001a3e561dc5f0d
Murhussain <husseinmurashi@yahoo.com>
2023-03-06 11:09:12 +0200

    Done with header section of the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'body': '#f6f6f6',
+        'gray-accent': '#989898',
+        'primary': '#870D22',
+      }
+    },
   },
   plugins: [],
 }
