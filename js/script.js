d59d053a9266343833a7b21a646ec652dae12b91
Murhussain <husseinmurashi@yahoo.com>
2023-03-09 10:05:00 +0200

    Adding custom scrollbar

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+///////////////////////////////////////////////////////////////////////////////////
+// Get all the accordion toggle buttons and add a click event listener to each one
+
+var accordionToggles = document.querySelectorAll('.accordion-toggle');
+
+accordionToggles.forEach(function(accordionToggle) {
+  accordionToggle.addEventListener('click', function() {
+    // Toggle the active class on the accordion toggle button
+    this.classList.toggle('active');
+    // Get the next sibling of the accordion header (which is the accordion content) and toggle the active class on it
+    var accordionContent = this.parentElement.nextElementSibling;
+    accordionContent.classList.toggle('active');
+  });
+});
